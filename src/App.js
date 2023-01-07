8d88a1c3a59bbb97c2263e8670cd60c75334efc2
unknown <117041148+amNotAWalnutToday@users.noreply.github.com>
2023-01-07 08:18:23 +0000

    Add template to store information in app state

--- src/App.js
+++ src/App.js
@@ -3,6 +3,44 @@ import Header from './components/Header';
 import PersonalInformation from './components/PersonalInformation';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      personalInfo: {
+        firstName: '',
+        lastName: '',
+        birthDate: '',
+        location: '',
+        email: '',
+        phoneNum: '',
+        description: '',
+      },
+
+      experience: [
+        {
+          /*Template*/
+          company: 'Software inc',
+          position: 'Software engineer',
+          startDate: '01/01/2021',
+          endDate: '01/01/2023',
+          extraInfo: 'N/A',
+        },
+      ],
+
+      education: [
+        {
+          /*Template*/
+          university: 'The University',
+          course: 'Computer Science',
+          startDate: '01/01/2017',
+          endDate: '01/01/2020',
+          extraInfo: 'N/A',
+        },
+      ],
+    };
+  }
+
   render(){
     return (
       <div>
